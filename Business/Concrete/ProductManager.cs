0853d27037a07772f3295c9ba69a534a7eb464e8
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-01-31 23:00:55 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _ProductDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _ProductDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _ProductDal.GetAll();
+        }
+    }
+}

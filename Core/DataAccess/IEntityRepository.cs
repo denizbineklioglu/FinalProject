b1ff23d84707bd52d70375192a09c0b3f0aeef4a
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-02-06 22:03:38 +0300

    6.2.2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
-
-namespace DataAccess.Abstract
+//CORE KATMANI DİĞER KATMANLARDAN REFERANS ALMAZ! ALIRSA O KATMANA BAĞIMLIDIR DEMEKTİR.
+namespace Core.DataAccess
 {
     //Generic Constraint
     // class : referans tip olabilir
--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
-    //IEntity implement eden class bir veritabanı tablosudur .
+    //IEntity implement eden class bir veritabanı tablosudur.
     public interface IEntity
     {
 

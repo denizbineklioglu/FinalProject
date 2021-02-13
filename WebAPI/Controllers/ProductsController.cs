75fc6d3487f3385e0f3ee2c4eee6a4457bd04798
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-02-13 22:18:42 +0300

    13.02.2021

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Attribute
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain -- bağımlılık zinciri
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+        }
+    }
+}

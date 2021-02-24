755b4303fb85bdfaa0f5d5265e43c3c3e3248eb8
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-02-24 22:04:06 +0300

    24.02

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,11 +19,9 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
-        ILogger _logger;
-        public ProductManager(IProductDal productDal,ILogger logger)
+        public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
-            _logger = logger;
         }
 
         [ValidationAspect(typeof(ProductValidator))]

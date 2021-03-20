f14813908bac878bdc485ea96daf71814919aa14
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-20 23:50:22 +0300

    last

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -14,7 +14,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p => p.ProductName).NotEmpty();
             RuleFor(p => p.ProductName).MinimumLength(2);
             RuleFor(p => p.UnitPrice).NotEmpty();
-            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10);
             RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
             RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
         }

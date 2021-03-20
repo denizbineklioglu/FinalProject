d1ee76dd76b3109bde8377d7ad69fbd242a10465
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-20 23:04:17 +0300

    last

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,7 +31,7 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
-        [SecuredOperation("admin")]
+        //[SecuredOperation("admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
